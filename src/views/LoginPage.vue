65bc7ed3aff7b3a124241e870fd28910995b02c3
fordsmiling <101375828+fordsmiling@users.noreply.github.com>
2024-03-04 11:25:06 +0700

    'pushcode'

--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,24 +1,27 @@
 <template>
-    <div>
-      <input type="email" v-model="email" placeholder="Email">
-      <input type="password" v-model="password" placeholder="Password">
-      <button @click="login">Login</button>
-    </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        email: '',
-        password: ''
-      };
-    },
-    methods: {
-      login() {
-        localStorage.setItem('token', 'ge9rgdfogpdfjpdfsd66dfdgf')
-        this.$router.push('/')
+  <div>
+    <input type="email" v-model="email" placeholder="Email" />
+    <input type="password" v-model="password" placeholder="Password" />
+    <button @click="login">Login</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: "",
+      password: "",
+    };
+  },
+  methods: {
+    async login() {
+      await localStorage.setItem("token", "jnouuiy54rfgftftyf766ftfytfgftfyf6ftfyf");
+      const token = localStorage.getItem("token");
+      if (token) {
+        this.$router.push("/");
       }
-    }
-  };
-  </script>
+    },
+  },
+};
+</script>
